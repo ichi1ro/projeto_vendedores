f90fc184a514fe97c40b7d9c3acd2e0b6a87180e
Raphael Ichiro <spookdrumbstep4200@gmail.com>
2024-04-15 11:25:56 -0300

    first commit

---
+++ Projeto_Vendedores/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_Vendedores.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
